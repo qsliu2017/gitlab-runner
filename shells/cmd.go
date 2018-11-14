324c27b37d75a68bc1efc880b91368e8b9131613
Jason <jlenny@gitlab.com>
2018-11-14 13:53:44 +0100

    attempt bash/ps output

--- shells/cmd.go
+++ shells/cmd.go
@@ -188,16 +188,28 @@ func (b *CmdWriter) Print(format string, arguments ...interface{}) {
 
 func (b *CmdWriter) Notice(format string, arguments ...interface{}) {
 	coloredText := helpers.ANSI_BOLD_GREEN + fmt.Sprintf(format, arguments...) + helpers.ANSI_RESET
+	timeStamp := "echo time /t\n"
+	b.Line("echo " + batchEscapeVariable("echo "+helpers.ANSI_BOLD_CYAN+"\n"))
+	b.Line("echo " + batchEscapeVariable(timeStamp))
+	b.Line("echo " + batchEscapeVariable("echo "+helpers.ANSI_RESET+"\n"))
 	b.Line("echo " + batchEscapeVariable(coloredText))
 }
 
 func (b *CmdWriter) Warning(format string, arguments ...interface{}) {
 	coloredText := helpers.ANSI_YELLOW + fmt.Sprintf(format, arguments...) + helpers.ANSI_RESET
+	timeStamp := "echo time /t\n"
+	b.Line("echo " + batchEscapeVariable("echo "+helpers.ANSI_BOLD_CYAN+"\n"))
+	b.Line("echo " + batchEscapeVariable(timeStamp))
+	b.Line("echo " + batchEscapeVariable("echo "+helpers.ANSI_RESET+"\n"))
 	b.Line("echo " + batchEscapeVariable(coloredText))
 }
 
 func (b *CmdWriter) Error(format string, arguments ...interface{}) {
 	coloredText := helpers.ANSI_BOLD_RED + fmt.Sprintf(format, arguments...) + helpers.ANSI_RESET
+	timeStamp := "echo time /t\n"
+	b.Line("echo " + batchEscapeVariable("echo "+helpers.ANSI_BOLD_CYAN+"\n"))
+	b.Line("echo " + batchEscapeVariable(timeStamp))
+	b.Line("echo " + batchEscapeVariable("echo "+helpers.ANSI_RESET+"\n"))
 	b.Line("echo " + batchEscapeVariable(coloredText))
 }
 
