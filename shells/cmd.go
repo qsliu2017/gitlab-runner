c45dae7def140587f8c60dc18b65d48429e78637
Jason <jlenny@gitlab.com>
2018-11-14 14:31:36 +0100

    try to fix cmd.exe

--- shells/cmd.go
+++ shells/cmd.go
@@ -189,21 +189,21 @@ func (b *CmdWriter) Print(format string, arguments ...interface{}) {
 func (b *CmdWriter) Notice(format string, arguments ...interface{}) {
 	coloredText := helpers.ANSI_BOLD_GREEN + fmt.Sprintf(format, arguments...) + helpers.ANSI_RESET
 	timeStamp := "time /t\n"
-	b.Line("echo " + batchEscapeVariable(timeStamp))
+	b.Line(batchEscapeVariable(timeStamp))
 	b.Line("echo " + batchEscapeVariable(coloredText))
 }
 
 func (b *CmdWriter) Warning(format string, arguments ...interface{}) {
 	coloredText := helpers.ANSI_YELLOW + fmt.Sprintf(format, arguments...) + helpers.ANSI_RESET
 	timeStamp := "time /t\n"
-	b.Line("echo " + batchEscapeVariable(timeStamp))
+	b.Line(batchEscapeVariable(timeStamp))
 	b.Line("echo " + batchEscapeVariable(coloredText))
 }
 
 func (b *CmdWriter) Error(format string, arguments ...interface{}) {
 	coloredText := helpers.ANSI_BOLD_RED + fmt.Sprintf(format, arguments...) + helpers.ANSI_RESET
 	timeStamp := "time /t\n"
-	b.Line("echo " + batchEscapeVariable(timeStamp))
+	b.Line(batchEscapeVariable(timeStamp))
 	b.Line("echo " + batchEscapeVariable(coloredText))
 }
 
