2bc11593d03e853163ebcbe5543122b42d34b78f
Kamil Trzciński <ayufan@ayufan.eu>
2019-05-24 13:41:47 +0200

    Make volumes to work on Linux Containers for Windows

---
+++ helpers/path/path.go
@@ -0,0 +1,8 @@
+package path
+
+type Path interface {
+	Join(elem ...string) string
+	IsAbs(path string) bool
+	IsRoot(path string) bool
+	Contains(basepath, targetpath string) bool
+}
---
+++ helpers/path/unix_path.go
@@ -0,0 +1,38 @@
+package path
+
+import "path"
+
+type unixPath struct{}
+
+func (p *unixPath) Join(elem ...string) string {
+	return path.Join(elem...)
+}
+
+func (p *unixPath) IsAbs(_path string) bool {
+	_path = path.Clean(_path)
+	return path.IsAbs(_path)
+}
+
+func (p *unixPath) IsRoot(_path string) bool {
+	_path = path.Clean(_path)
+	return path.IsAbs(_path) && path.Dir(_path) == _path
+}
+
+func (p *unixPath) Contains(basepath, targetpath string) bool {
+	basepath = path.Clean(basepath)
+	targetpath = path.Clean(targetpath)
+
+	for {
+		if targetpath == basepath {
+			return true
+		}
+		if p.IsRoot(targetpath) || targetpath == "." {
+			return false
+		}
+		targetpath = path.Dir(targetpath)
+	}
+}
+
+func NewUnixPath() Path {
+	return &unixPath{}
+}
