911ac81b593556f200fe0f898d24c8793b5127f7
Tomasz Maczukin <tomasz@maczukin.pl>
2019-05-31 19:31:59 +0200

    Add more logging around exitcode check in PS

--- shells/powershell.go
+++ shells/powershell.go
@@ -69,7 +69,14 @@ func (b *PsWriter) Unindent() {
 }
 
 func (b *PsWriter) checkErrorLevel() {
-	b.Line("if(!$?) { Exit $LASTEXITCODE }")
+	b.Line("if(!$?) {")
+	b.Indent()
+	b.Line("$lecg=$?")
+	b.Line("$lec=$LASTEXITCODE")
+	b.Warning("exiting with lecg=$lecg lec=$lec")
+	b.Line("Exit $lec")
+	b.Unindent()
+	b.Line("}")
 	b.Line("")
 }
 
