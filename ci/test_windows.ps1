7f64d27b9bc92393b280d0bfd3f189ec20225368
Tomasz Maczukin <tomasz@maczukin.pl>
2019-07-03 00:49:16 +0200

    Add subsections to Windows tests output

--- ci/test_windows.ps1
+++ ci/test_windows.ps1
@@ -24,9 +24,19 @@ $failed = @()
 Get-Content $testsDefinitionsFile | Select-Object -skip $executionOffset -first $executionSize | ForEach-Object {
     $pkg, $index, $tests = $_.Split(" ", 3)
 
-    Write-Information "`r`n`r`n--- Starting part $index of go tests of '$pkg' package:`r`n`r`n"
+    $SectionName=((echo "test_group_${pkg}_${index}" | % { $_ -replace "[^a-z0-9_]","_" }))
+
+    $DateTimeStart = (Get-Date).ToUniversalTime()
+    $UnixTimeStampStart = [System.Math]::Truncate((Get-Date -Date $DateTimeStart -UFormat %s))
+    Write-Information "`r`n`r`nsection_start:${UnixTimeStampStart}:${SectionName}\r\033[0K"
+
+    Write-Information "--- Starting part $index of go tests of '$pkg' package:`r`n`r`n"
 
     go test -v $pkg -run "$tests"
+
+    $DateTimeEnd = (Get-Date).ToUniversalTime()
+    $UnixTimeStampEnd = [System.Math]::Truncate((Get-Date -Date $DateTimeEnd -UFormat %s))
+    Write-Information "`r`n`r`nsection_end:${UnixTimeStampEnd}:${SectionName}\r\033[0K"
 
     if ($LASTEXITCODE -ne 0) {
         $failed += "$pkg-$index"
