7d10a0214c3a67d2dba9b687ba11bb40a188f097
Alex Groleau <agroleau@gitlab.com>
2019-10-15 16:33:07 -0400

    fixed prometheus api mocking class

--- network/mock_prometheus_api.go
+++ network/mock_prometheus_api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/prometheus/client_golang/api"
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 
 	"github.com/prometheus/common/model"
@@ -12,6 +13,10 @@ import (
 
 type MockPrometheusApi struct {
 	mock.Mock
+}
+
+func (mpi *MockPrometheusApi) Alerts(ctx context.Context) (v1.AlertsResult, error) {
+	return v1.AlertsResult{}, nil
 }
 
 func (mpi *MockPrometheusApi) AlertManagers(ctx context.Context) (v1.AlertManagersResult, error) {
@@ -34,15 +39,19 @@ func (mpi *MockPrometheusApi) Flags(ctx context.Context) (v1.FlagsResult, error)
 	return nil, nil
 }
 
-func (mpi *MockPrometheusApi) LabelValues(ctx context.Context, label string) (model.LabelValues, error) {
-	return nil, nil
+func (mpi *MockPrometheusApi) LabelNames(ctx context.Context) ([]string, api.Warnings, error) {
+	return nil, nil, nil
 }
 
-func (mpi *MockPrometheusApi) Query(ctx context.Context, query string, ts time.Time) (model.Value, error) {
-	return nil, nil
+func (mpi *MockPrometheusApi) LabelValues(ctx context.Context, label string) (model.LabelValues, api.Warnings, error) {
+	return nil, nil, nil
 }
 
-func (mpi *MockPrometheusApi) QueryRange(ctx context.Context, query string, r v1.Range) (model.Value, error) {
+func (mpi *MockPrometheusApi) Query(ctx context.Context, query string, ts time.Time) (model.Value, api.Warnings, error) {
+	return nil, nil, nil
+}
+
+func (mpi *MockPrometheusApi) QueryRange(ctx context.Context, query string, r v1.Range) (model.Value, api.Warnings, error) {
 	metric := model.Metric{"test": "test"}
 	var ts model.Time = 32432
 	var sv model.SampleValue = 435345
@@ -50,11 +59,15 @@ func (mpi *MockPrometheusApi) QueryRange(ctx context.Context, query string, r v1
 	sps := []model.SamplePair{samplePair}
 	sampleStream := model.SampleStream{Metric: metric, Values: sps}
 	var matrix model.Matrix = []*model.SampleStream{&sampleStream}
-	return matrix, nil
+	return matrix, nil, nil
 }
 
-func (mpi *MockPrometheusApi) Series(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) ([]model.LabelSet, error) {
-	return nil, nil
+func (mpi *MockPrometheusApi) Rules(ctx context.Context) (v1.RulesResult, error) {
+	return v1.RulesResult{}, nil
+}
+
+func (mpi *MockPrometheusApi) Series(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) ([]model.LabelSet, api.Warnings, error) {
+	return nil, nil, nil
 }
 
 func (mpi *MockPrometheusApi) Snapshot(ctx context.Context, skipHead bool) (v1.SnapshotResult, error) {
@@ -63,4 +76,8 @@ func (mpi *MockPrometheusApi) Snapshot(ctx context.Context, skipHead bool) (v1.S
 
 func (mpi *MockPrometheusApi) Targets(ctx context.Context) (v1.TargetsResult, error) {
 	return v1.TargetsResult{}, nil
+}
+
+func (mpi *MockPrometheusApi) TargetsMetadata(ctx context.Context, matchTarget string, metric string, limit string) ([]v1.MetricMetadata, error) {
+	return nil, nil
 }
