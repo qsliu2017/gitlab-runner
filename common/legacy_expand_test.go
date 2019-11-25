36a8a34c0ebe8e7269358bc3dd26a9ce0926237e
Stan Hu <stanhu@gmail.com>
2019-11-25 23:51:43 +0000

    Backport os.Expand() from Go v1.10.8
    This is needed because Go v1.11+ changed the behavior in
    https://github.com/golang/go/commit/7bf631e1fc55839a6bff404a91597326e1addc10#diff-b30ffd1cf7612ccebac97fced83d5106
    so that `$` isn't gobbled unless the value is a valid shell variable
    (e.g. `$FOOBAR`, `${FOOBAR}`).
    
    Since this may break CI builds that rely on this legacy behavior, keep
    it for now.

---
+++ common/legacy_expand_test.go
@@ -0,0 +1,61 @@
+// Backported from Go v1.10.8:
+// https://github.com/golang/go/blob/8623503fe54642a21854c551129d550139f3bbac/src/os/env_test.go
+
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package common
+
+import (
+	"testing"
+)
+
+// testGetenv gives us a controlled set of variables for testing Expand.
+func testGetenv(s string) string {
+	switch s {
+	case "*":
+		return "all the args"
+	case "#":
+		return "NARGS"
+	case "$":
+		return "PID"
+	case "1":
+		return "ARGUMENT1"
+	case "HOME":
+		return "/usr/gopher"
+	case "H":
+		return "(Value of H)"
+	case "home_1":
+		return "/usr/foo"
+	case "_":
+		return "underscore"
+	}
+	return ""
+}
+
+var expandTests = []struct {
+	in, out string
+}{
+	{"", ""},
+	{"$*", "all the args"},
+	{"$$", "PID"},
+	{"${*}", "all the args"},
+	{"$1", "ARGUMENT1"},
+	{"${1}", "ARGUMENT1"},
+	{"now is the time", "now is the time"},
+	{"$HOME", "/usr/gopher"},
+	{"$home_1", "/usr/foo"},
+	{"${HOME}", "/usr/gopher"},
+	{"${H}OME", "(Value of H)OME"},
+	{"A$$$#$1$H$home_1*B", "APIDNARGSARGUMENT1(Value of H)/usr/foo*B"},
+}
+
+func TestLegacyExpand(t *testing.T) {
+	for _, test := range expandTests {
+		result := LegacyExpand(test.in, testGetenv)
+		if result != test.out {
+			t.Errorf("Expand(%q)=%q; expected %q", test.in, result, test.out)
+		}
+	}
+}
