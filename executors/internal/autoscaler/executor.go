1f67dfd0fe341d81a25edb7894d7b9dd98823b06
Joseph Burnett <jburnett@gitlab.com>
2023-05-06 16:42:31 -0700

    Add hold and release endpoints

--- executors/internal/autoscaler/executor.go
+++ executors/internal/autoscaler/executor.go
@@ -23,8 +23,33 @@ type executor struct {
 	config   common.RunnerConfig
 }
 
+type job struct {
+	acquisition taskscaler.Acquisition
+	holdUntil   time.Time
+}
+
 var jobsMux = sync.Mutex{}
-var jobs = map[int64]taskscaler.Acquisition{}
+var jobs = map[int64]*job{}
+
+func getJob(jobID int64) *job {
+	jobsMux.Lock()
+	defer jobsMux.Unlock()
+	j, ok := jobs[jobID]
+	if !ok {
+		j = &job{}
+		jobs[jobID] = j
+	}
+	return j
+}
+
+func deleteJob(jobID int64) {
+	jobsMux.Lock()
+	defer jobsMux.Unlock()
+	_, ok := jobs[jobID]
+	if ok {
+		delete(jobs, jobID)
+	}
+}
 
 func (e *executor) Prepare(options common.ExecutorPrepareOptions) (err error) {
 	e.build = options.Build
@@ -52,10 +77,8 @@ func (e *executor) Prepare(options common.ExecutorPrepareOptions) (err error) {
 		return fmt.Errorf("unable to acquire instance: %w", err)
 	}
 
-	jobsMux.Lock()
-	jobID := options.Build.JobResponse.ID
-	jobs[jobID] = acq
-	jobsMux.Unlock()
+	j := getJob(options.Build.JobResponse.ID)
+	j.acquisition = acq
 
 	e.build.Log().WithField("key", acqRef.key).Trace("Acquired capacity...")
 
@@ -65,28 +88,71 @@ func (e *executor) Prepare(options common.ExecutorPrepareOptions) (err error) {
 }
 
 func (e *executor) Cleanup() {
-	jobsMux.Lock()
 	jobID := e.build.JobResponse.ID
-	delete(jobs, jobID)
-	jobsMux.Unlock()
-	e.Executor.Cleanup()
+	j := getJob(jobID)
+	for {
+		if time.Now().After(j.holdUntil) {
+			deleteJob(jobID)
+			e.Executor.Cleanup()
+			return
+		}
+		time.Sleep(time.Second)
+	}
 }
 
 func init() {
 	go func() {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.HandleFunc("/hold", func(w http.ResponseWriter, r *http.Request) {
 			errorResponse := func(msg string) {
 				w.WriteHeader(http.StatusInternalServerError) // whatever
 				w.Write([]byte(msg))
 			}
 			jobIDString := r.URL.Query().Get("jobID")
 			if jobIDString == "" {
-				errorResponse("jobID not provided")
+				errorResponse("hold: jobID not provided")
 				return
 			}
 			jobID, err := strconv.Atoi(jobIDString)
 			if err != nil {
-				errorResponse("invalid jobID: " + jobIDString)
+				errorResponse("hold: invalid jobID: " + jobIDString)
+				return
+			}
+			j := getJob(int64(jobID))
+			j.holdUntil = time.Now().Add(30 * time.Minute)
+			w.WriteHeader(http.StatusOK)
+		})
+		http.HandleFunc("/release", func(w http.ResponseWriter, r *http.Request) {
+			errorResponse := func(msg string) {
+				w.WriteHeader(http.StatusInternalServerError) // whatever
+				w.Write([]byte(msg))
+			}
+			jobIDString := r.URL.Query().Get("jobID")
+			if jobIDString == "" {
+				errorResponse("release: jobID not provided")
+				return
+			}
+			jobID, err := strconv.Atoi(jobIDString)
+			if err != nil {
+				errorResponse("release: invalid jobID: " + jobIDString)
+				return
+			}
+			j := getJob(int64(jobID))
+			j.holdUntil = time.Now()
+			w.WriteHeader(http.StatusOK)
+		})
+		http.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
+			errorResponse := func(msg string) {
+				w.WriteHeader(http.StatusInternalServerError) // whatever
+				w.Write([]byte(msg))
+			}
+			jobIDString := r.URL.Query().Get("jobID")
+			if jobIDString == "" {
+				errorResponse("connect: jobID not provided")
+				return
+			}
+			jobID, err := strconv.Atoi(jobIDString)
+			if err != nil {
+				errorResponse("connect: invalid jobID: " + jobIDString)
 				return
 			}
 			body, err := ioutil.ReadAll(r.Body)
@@ -95,14 +161,12 @@ func init() {
 				return
 			}
 			publicKey := strings.TrimSpace(string(body))
-			jobsMux.Lock()
-			acq, ok := jobs[int64(jobID)]
-			jobsMux.Unlock()
-			if !ok {
-				errorResponse("jobID not found")
+			j := getJob(int64(jobID))
+			if j.acquisition == nil {
+				errorResponse("connect: no acquisition yet")
 				return
 			}
-			connectInfo, err := acq.InstanceConnectInfo(context.Background(), publicKey)
+			connectInfo, err := j.acquisition.InstanceConnectInfo(context.Background(), publicKey)
 			if err != nil {
 				errorResponse(err.Error())
 				return
